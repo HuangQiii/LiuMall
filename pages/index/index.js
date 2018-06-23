dba9e2dd7ed1343770a25106480e1b2fcd4c67a2
HuangQiii <598260403@qq.com>
2018-06-23 22:00:30 +0800

    [FIX] fix index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,148 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello Liu',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    banner: [
+      {
+        id: 1,
+        type: 0,
+        linkId: 1,
+        bannerName: '',
+        imageUrl: 'https://img.alicdn.com/tps/i4/TB1ADXlqLiSBuNkSnhJSuvDcpXa.jpg_490x490q100.jpg_.webp'
+      },
+      {
+        id: 2,
+        type: 1,
+        linkId: 2,
+        bannerName: '',
+        imageUrl: 'https://img.alicdn.com/imgextra/i2/2671315119/TB2b2zabvNNTKJjSspeXXaSwpXa_!!2671315119.jpg_490x490q100.jpg_.webp'
+      },
+      {
+        id: 3,
+        type: 0,
+        linkId: 3,
+        bannerName: '',
+        imageUrl: 'https://img.alicdn.com/tfs/TB1vvvJyFOWBuNjy0FiXXXFxVXa-468-644.jpg_320x5000q100.jpg_.webp'
+      }
+    ],
+    channel: [
+      {
+        id: '1',
+        type: 0,
+        linkId: '3',
+        iconUrl: 'close',
+        name: 'Type1'
+      },
+      {
+        id: '1',
+        type: 0,
+        linkId: '3',
+        iconUrl: 'location',
+        name: 'Type2'
+      },
+      {
+        id: '3',
+        type: 1,
+        linkId: '3',
+        iconUrl: 'clock',
+        name: 'Type3'
+      },
+      {
+        id: '4',
+        type: 1,
+        linkId: '4',
+        iconUrl: 'chat',
+        name: 'Type4'
+      },
+      {
+        id: '5',
+        type: 1,
+        linkId: '5',
+        iconUrl: 'exchange',
+        name: 'Type5'
+      },
+      {
+        id: '6',
+        type: 0,
+        linkId: '6',
+        iconUrl: 'upgrade',
+        name: 'Type6'
+      },
+      {
+        id: '7',
+        type: 0,
+        linkId: '7',
+        iconUrl: 'edit',
+        name: 'Type7'
+      },
+      {
+        id: '8',
+        type: 1,
+        linkId: '8',
+        iconUrl: 'contact',
+        name: 'Type8'
+      },
+      {
+        id: '9',
+        type: 1,
+        linkId: '9',
+        iconUrl: 'passed',
+        name: 'Type9'
+      },
+      {
+        id: '10',
+        type: 1,
+        linkId: '10',
+        iconUrl: 'records',
+        name: 'Type10'
+      }
+    ],
+    brand: [
+      {
+        picUrl: '../../images/wudong.jpg',
+        name: '产品1',
+        description: '产品1的描述',
+        price: '1.00'
+      },
+      {
+        picUrl: '../../images/wudong.jpg',
+        name: '产品2',
+        description: '产品2的描述',
+        price: '1.00'
+      },
+      {
+        picUrl: '../../images/wudong.jpg',
+        name: '产品3',
+        description: '产品3的描述',
+        price: '1.00'
+      },
+      {
+        picUrl: '../../images/wudong.jpg',
+        name: '产品4',
+        description: '产品4的描述',
+        price: '1.00'
+      }
+    ]
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          console.log(res)
+        } else {
+          console.log('登录失败！' + res.errMsg)
         }
-      })
-    }
+      }
+    });
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  bannerNav: function (e) {
+    let banner = e.target.dataset.value;
+    wx.navigateTo({
+      url: '/pages/goods/goods',
+    })
+  },
+  channelNav: function (e) {
+    let channel = e.target.dataset.value;
+    wx.navigateTo({
+      url: '/pages/category/category',
     })
   }
 })
