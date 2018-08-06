6e96adcf87573a8792e0104177d0a6d3fc0adcba
HuangQiii <598260403@qq.com>
2018-08-06 23:10:02 +0800

    [FIX] add temp touch

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -2,6 +2,7 @@ var app = getApp();
 
 Page({
   data: {
+    delBtnWidth: 180,
     cartGoods: [
       {
         id: '1',
@@ -11,34 +12,38 @@ Page({
         spec: '啦啦啦啦啦啦啦啦啦啦啦',
         number: 1,
         price: '688.00',
+        txtStyle: "",
       },
-      {
-        id: '2',
-        checked: true,
-        picUrl: '../../images/wudong3.jpg',
-        name: '要怎样',
-        spec: '哈哈哈哈哈哈哈哈哈哈',
-        number: 1,
-        price: '888.00',
-      },
-      {
-        id: '3',
-        checked: false,
-        picUrl: '../../images/wudong.jpg',
-        name: '你还要我怎样',
-        spec: '啦啦啦啦啦啦啦啦啦啦啦',
-        number: 1,
-        price: '688.00',
-      },
-      {
-        id: '4',
-        checked: false,
-        picUrl: '../../images/wudong4.jpg',
-        name: '要怎样',
-        spec: '哈哈哈哈哈哈哈哈哈哈',
-        number: 1,
-        price: '888.00',
-      }
+      // {
+      //   id: '2',
+      //   checked: true,
+      //   picUrl: '../../images/wudong3.jpg',
+      //   name: '要怎样',
+      //   spec: '哈哈哈哈哈哈哈哈哈哈',
+      //   number: 1,
+      //   price: '888.00',
+      //   txtStyle: "",
+      // },
+      // {
+      //   id: '3',
+      //   checked: false,
+      //   picUrl: '../../images/wudong.jpg',
+      //   name: '你还要我怎样',
+      //   spec: '啦啦啦啦啦啦啦啦啦啦啦',
+      //   number: 1,
+      //   price: '688.00',
+      //   txtStyle: "",
+      // },
+      // {
+      //   id: '4',
+      //   checked: false,
+      //   picUrl: '../../images/wudong4.jpg',
+      //   name: '要怎样',
+      //   spec: '哈哈哈哈哈哈哈哈哈哈',
+      //   number: 1,
+      //   price: '888.00',
+      //   txtStyle: "",
+      // }
     ],
     checkedGoodsCount: 2,
     checkedGoodsAmount: '',
@@ -133,5 +138,99 @@ Page({
   },
   checkOut: function () {
     // check out
-  }
+  },
+  touchS: function (e) {
+    if (e.touches.length == 1) {
+      this.setData({
+        //设置触摸起始点水平方向位置
+        startX: e.touches[0].clientX
+      });
+    }
+  },
+  touchM: function (e) {
+    if (e.touches.length == 1) {
+      //手指移动时水平方向位置
+      var moveX = e.touches[0].clientX;
+      //手指起始点位置与移动期间的差值
+      var disX = this.data.startX - moveX;
+      var delBtnWidth = this.data.delBtnWidth;
+      var txtStyle = "";
+      if (disX == 0 || disX < 0) {//如果移动距离小于等于0，文本层位置不变
+        txtStyle = "left:0px";
+      } else if (disX > 0) {//移动距离大于0，文本层left值等于手指移动距离
+        txtStyle = "left:-" + disX + "px";
+        if (disX >= delBtnWidth) {
+          //控制手指移动距离最大值为删除按钮的宽度
+          txtStyle = "left:-" + delBtnWidth + "px";
+        }
+      }
+      //获取手指触摸的是哪一项
+      var id = e.target.dataset.index;
+      var cartGoods = this.data.cartGoods;
+      const index = cartGoods.findIndex(v => v.id === id);
+      if(index > -1) {
+        cartGoods[index].txtStyle = txtStyle;
+        //更新列表的状态
+        this.setData({
+          cartGoods: cartGoods,
+        });
+      }
+    }
+  },
+
+  touchE: function (e) {
+    if (e.changedTouches.length == 1) {
+      //手指移动结束后水平位置
+      var endX = e.changedTouches[0].clientX;
+      //触摸开始与结束，手指移动的距离
+      var disX = this.data.startX - endX;
+      var delBtnWidth = this.data.delBtnWidth;
+      //如果距离小于删除按钮的1/2，不显示删除按钮
+      var txtStyle = disX > delBtnWidth / 2 ? "left:-" + delBtnWidth + "px" : "left:0px";
+      //获取手指触摸的是哪一项
+      var id = e.target.dataset.index;
+      var cartGoods = this.data.cartGoods;
+      const index = cartGoods.findIndex(v => v.id === id);
+      if (index >= 0) {
+        cartGoods[index].txtStyle = txtStyle;
+        //更新列表的状态
+        this.setData({
+          cartGoods: cartGoods,
+        });
+      }
+    }
+  },
+  //获取元素自适应后的实际宽度
+  getEleWidth: function (w) {
+    var real = 0;
+    try {
+      var res = wx.getSystemInfoSync().windowWidth;
+      var scale = (750 / 2) / (w / 2);//以宽度750px设计稿做宽度的自适应
+      // console.log(scale);
+      real = Math.floor(res / scale);
+      return real;
+    } catch (e) {
+      return false;
+      // Do something when catch error
+    }
+  },
+  initEleWidth: function () {
+    var delBtnWidth = this.getEleWidth(this.data.delBtnWidth);
+    this.setData({
+      delBtnWidth: delBtnWidth
+    });
+  },
+  //点击删除按钮事件
+  delItem: function (e) {
+    //获取列表中要删除项的下标
+    var index = e.target.dataset.index;
+    var list = this.data.list;
+    //移除列表中下标为index的项
+    list.splice(index, 1);
+    //更新列表的状态
+    this.setData({
+      list: list
+    });
+  },
+
 })
